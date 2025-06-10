d211011ab52143cc07a6715f0bfcf3d3f469b081
munir <smsirajulmonir@gmail.com>
2025-06-10 15:26:32 +0600

    AND OR filtering explored

---
+++ src/filtering.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient();
+
+const filtering = async() =>{
+    const andFiltering = await prisma.post.findMany({
+        where:{
+            AND:[
+                {
+                    title:{
+                        contains:"title"
+                    },
+                },
+                {
+                    published:true
+                }
+            ]
+        }
+    })
+    const orFiltering = await prisma.post.findMany({
+        where:{
+            OR:[
+                {
+                    title:{
+                        contains:"title"
+                    }
+                },
+                {
+                    published:true
+                }
+            ]
+        }
+    })
+    console.log("Or Filtering",orFiltering);
+}
+
+filtering();

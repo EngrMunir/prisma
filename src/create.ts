a56869382a50306c13dae2a0ee62a4c515fbafff
munir <smsirajulmonir@gmail.com>
2025-06-10 15:03:07 +0600

    fluent api explored

--- src/create.ts
+++ src/create.ts
@@ -1,29 +1,67 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient, UserRole } from "../generated/prisma";
 
 
 const prisma = new PrismaClient();
 
 const main = async()=>{
-    const result = await prisma.post.createMany({
-        data:[
-            {
-            title: "This is title 1",
-            content: "This is author 1",
-            authorName: "Munir 1"
-            },
-            {
-            title: "This is title 2",
-            content: "This is author 2",
-            authorName: "Munir 2"
-            },
-            {
-            title: "This is title 3",
-            content: "This is author 3",
-            authorName: "Munir 3"
-            },
-        ]
-    });
-    console.log(result)
+    
+    // const createUser = await prisma.user.create({
+    //     data:{
+    //         username:'user3',
+    //         email:'user3@gmail.com',
+    //         role: UserRole.user
+    //     }
+    // });
+    // console.log(createUser)
+
+    // const createProfile = await prisma.profile.create({
+    //     data:{
+    //         bio:"This is bio.....",
+    //         userId:1
+    //     }
+    // })
+    // console.log(createProfile)
+
+    // const createCategory = await prisma.category.create({
+    //     data:{
+    //         name:"Software Engineering"
+    //     }
+    // })
+    // console.log(createCategory)
+
+    const createPost = await prisma.post.create({
+        data:{
+            title:"This is title",
+            content:"This is content of the post 5",
+            authorId: 2,
+            postCategory: {
+                create:[
+                    {
+                        categoryId: 1
+                    },
+                    {
+                        categoryId: 3
+                    },
+                    {
+                        categoryId: 2
+                    },
+                    {
+                        categoryId: 4
+                    }
+                ]
+                // create:{
+                //     categoryId: 2
+                //     // category:{
+                //     //     connect:{
+                //     //         id: 1
+                //     //     }
+                //     // }
+                // }
+            }
+
+        }
+    })
+    console.log(createPost);
 }
 
 main();
